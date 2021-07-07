a764b7d7e56fef8414f83defdd1a50c859f673b7
Geoffrey Roberts <groberts314@yahoo.com>
2021-07-07 02:40:27 -0700

    feat: add extension method to adjust numbers for normal balance type

---
+++ src/Extensions/DecimalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using DashAccountingSystemV2.Models;
+
+namespace DashAccountingSystemV2.Extensions
+{
+    public static class DecimalExtensions
+    {
+        public static decimal WithNormalBalanceType(this decimal amount, AmountType normalBalanceType)
+        {
+            switch (normalBalanceType)
+            {
+                case AmountType.Credit:
+                    return amount * -1;
+
+                case AmountType.Debit:
+                default:
+                    return amount;
+            }
+        }
+    }
+}
---
+++ test/DashAccountingSystemV2.Tests/Extensions/DecimalExtensionsFixture.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using DashAccountingSystemV2.Extensions;
+using DashAccountingSystemV2.Models;
+
+namespace DashAccountingSystemV2.Tests.Extensions
+{
+    public class DecimalExtensionsFixture
+    {
+        [Theory]
+        [InlineData(1000, AmountType.Debit, 1000)]
+        [InlineData(-1000, AmountType.Debit, -1000)]
+        [InlineData(-2500.25, AmountType.Credit, 2500.25)]
+        [InlineData(2500.25, AmountType.Credit, -2500.25)]
+        public void WithNormalBalanceType_Ok(decimal originalAmount, AmountType normalBalanceType, decimal expectedAmount)
+        {
+            Assert.Equal(expectedAmount, originalAmount.WithNormalBalanceType(normalBalanceType));
+        }
+    }
+}
