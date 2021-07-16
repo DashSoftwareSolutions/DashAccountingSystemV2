64ae4486c0e0364647de4c39b987bbc85e40af60
Geoffrey Roberts <groberts314@yahoo.com>
2021-07-16 04:57:13 -0700

    feat: [Back-End] - API to fetch list of Employees
    Resolves #43

---
+++ src/BusinessLogic/IEmployeeBusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DashAccountingSystemV2.Models;
+
+namespace DashAccountingSystemV2.BusinessLogic
+{
+    public interface IEmployeeBusinessLogic
+    {
+        Task<BusinessLogicResponse<IEnumerable<Employee>>> GetByTenant(
+            Guid tenantId,
+            IEnumerable<uint> employeeNumbers = null,
+            bool onlyActive = true);
+
+        // TODO: Add GetDetaildById() and CUD methods as needed
+    }
+}
