936b758230d9866ba8fc83316bbe30e8d222172b
Geoffrey Roberts <groberts314@yahoo.com>
2022-02-20 03:00:59 -0800

    feat: UI models for single detailed Invoice CRUD
    In service of #61

---
+++ src/ClientApp/src/models/InvoiceTerms.ts
@@ -0,0 +1,9 @@
+﻿export default interface InvoiceTerms {
+    id: string | null; // GUID
+    tenantId: string | null; // GUID
+    isCustom: boolean;
+    name: string | null;
+    dueInDays: number | null;
+    dueOnDayOfMonth: number | null;
+    dueNextMonthThreshold: number | null;
+}
