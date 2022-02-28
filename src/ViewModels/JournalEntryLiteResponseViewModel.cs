996adb55a7c89113979f9ac4015fbce319924589
Geoffrey Roberts <groberts314@yahoo.com>
2022-02-28 04:18:17 -0800

    feat: add endpoint to create (POST) a Payment
    Resolves #65

---
+++ src/ViewModels/JournalEntryLiteResponseViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using DashAccountingSystemV2.Models;
+using DashAccountingSystemV2.ViewModels.Serialization;
+
+namespace DashAccountingSystemV2.ViewModels
+{
+    public class JournalEntryLiteResponseViewModel : INumberedJournalEntry
+    {
+        public Guid Id { get; set; }
+
+        public uint EntryId { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public TransactionStatus Status { get; set; }
+
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime EntryDate { get; set; }
+
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime? PostDate { get; set; }
+
+        public string Description { get; set; }
+
+        public static JournalEntryLiteResponseViewModel FromModel(JournalEntry model)
+        {
+            if (model == null)
+                return null;
+
+            return new JournalEntryLiteResponseViewModel()
+            {
+                Id = model.Id,
+                EntryId = model.EntryId,
+                EntryDate = model.EntryDate,
+                Description = model.Description,
+                PostDate = model.PostDate,
+                Status = model.Status,
+            };
+        }
+    }
+}
---
+++ src/ViewModels/PaymentResponseViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using DashAccountingSystemV2.Extensions;
+using DashAccountingSystemV2.Models;
+using DashAccountingSystemV2.ViewModels.Serialization;
+
+namespace DashAccountingSystemV2.ViewModels
+{
+    public class PaymentResponseViewModel
+    {
+        public Guid Id { get; set; }
+
+        public Guid TenantId { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public CustomerLiteResponseViewModel Customer { get; set; }
+
+        public Guid DepositAccountId { get; set; }
+
+        public AccountLiteResponseViewModel DepositAccount { get; set; }
+
+        public Guid RevenueAccountId { get; set; }
+
+        public AccountLiteResponseViewModel RevenueAccount { get; set; }
+
+        public int PaymentMethodId { get; set; }
+
+        public LookupValueViewModel PaymentMethod { get; set; }
+
+        public uint? CheckNumber { get; set; }
+
+        [JsonConverter(typeof(JsonDateConverter))]
+        public DateTime PaymentDate { get; set; }
+
+        public AmountViewModel Amount { get; set; }
+
+        public Guid JournalEntryId { get; set; }
+
+        public JournalEntryLiteResponseViewModel JournalEntry { get; set; }
+
+        public IEnumerable<InvoicePaymentResponseViewModel> Invoices { get; set; }
+
+        [JsonConverter(typeof(JsonIsoDateTimeConverter))]
+        public DateTime Created { get; set; }
+
+        public ApplicationUserLiteViewModel CreatedBy { get; set; }
+
+        [JsonConverter(typeof(JsonIsoDateTimeConverter))]
+        public DateTime? Updated { get; set; }
+
+        public ApplicationUserLiteViewModel UpdatedBy { get; set; }
+
+        public static PaymentResponseViewModel FromModel(Payment model)
+        {
+            if (model == null)
+                return null;
+
+            return new PaymentResponseViewModel()
+            {
+                Id = model.Id,
+                TenantId = model.TenantId,
+                CustomerId = model.CustomerId,
+                Customer = CustomerLiteResponseViewModel.FromModel(model.Customer),
+                DepositAccountId = model.DepositAccountId,
+                DepositAccount = AccountLiteResponseViewModel.FromModel(model.DepositAccount),
+                RevenueAccountId = model.RevenueAccountId,
+                RevenueAccount = AccountLiteResponseViewModel.FromModel(model.RevenueAccount),
+                PaymentMethodId = model.PaymentMethodId,
+                PaymentMethod = model.PaymentMethod != null ?
+                    new LookupValueViewModel(model.PaymentMethod.Id, model.PaymentMethod.Name) :
+                    null,
+                CheckNumber = model.CheckNumber,
+                PaymentDate = model.Date,
+                Amount = new AmountViewModel(model.Amount, model.AssetType),
+                JournalEntryId = model.JournalEntryId,
+                JournalEntry = JournalEntryLiteResponseViewModel.FromModel(model.JournalEntry),
+                Invoices = model.Invoices.Select(i => InvoicePaymentResponseViewModel.FromModel(i, model.AssetType)),
+                Created = model.Created.AsUtc(),
+                CreatedBy = ApplicationUserLiteViewModel.FromModel(model.CreatedBy),
+                Updated = model.Updated.AsUtc(),
+                UpdatedBy = ApplicationUserLiteViewModel.FromModel(model.UpdatedBy),
+            };
+        }
+    }
+}
