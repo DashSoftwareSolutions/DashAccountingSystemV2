3ae2eec6cc75333aadb378ebef9d23f0d18056e3
Geoffrey Roberts <groberts314@yahoo.com>
2022-03-13 05:47:27 -0700

    feat: update `TenantRepository` get single methods to hydrate address
    * In service of #88
    * Converted EF queries into Dapper queries, since EF does not know how to navigate Tenant => Tenant Entity => Address
    * Updated Tenant Repository unit test fixture to create Entity and Address.  Need to add some more assertions, but this will do for now.

--- test/DashAccountingSystemV2.Tests/Repositories/TenantRepositoryFixture.cs
+++ test/DashAccountingSystemV2.Tests/Repositories/TenantRepositoryFixture.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Dapper;
 using Npgsql;
 using Xunit;
+using DashAccountingSystemV2.Models;
 using DashAccountingSystemV2.Repositories;
 
 namespace DashAccountingSystemV2.Tests.Repositories
@@ -70,10 +71,66 @@ namespace DashAccountingSystemV2.Tests.Repositories
 
             using (var connection = new NpgsqlConnection(connString))
             {
-                _tenantId = await connection.QueryFirstOrDefaultAsync<Guid>($@"
-                    INSERT INTO ""Tenant"" ( ""Name"" )
-                    VALUES ( '{_tenantName}' )
-                    RETURNING ""Id"";");
+                var assetTypeUSD = await connection.QueryFirstOrDefaultAsync<int>(
+                    @"SELECT ""Id"" FROM ""AssetType"" WHERE ""Name"" = 'USD';");
+
+                var userId = await connection.QueryFirstOrDefaultAsync<Guid>(
+                    @"SELECT ""Id"" FROM ""AspNetUsers"" ORDER BY ""LastName"", ""FirstName"" LIMIT 1;");
+
+                _tenantId = await connection.QueryFirstOrDefaultAsync<Guid>(
+@"INSERT INTO ""Tenant"" ( ""Name"", ""DefaultAssetTypeId"", ""ContactEmailAddress"" )
+VALUES ( @_tenantName, @assetTypeUSD, 'someone@example.com' )
+RETURNING ""Id"";",
+                    new { _tenantName, assetTypeUSD });
+
+                var tenantEntityId = await connection.QueryFirstOrDefaultAsync<Guid>(
+@"INSERT INTO ""Entity"" ( ""TenantId"", ""EntityType"", ""CreatedById"" )
+VALUES ( @_tenantId, @entityType, @userId )
+RETURNING ""Id"";",
+                    new { _tenantId, userId, entityType = (int)EntityType.Tenant });
+
+                var countryUSA = await connection.QueryFirstOrDefaultAsync<int>(
+                    @"SELECT ""Id"" FROM ""Country"" WHERE ""ThreeLetterCode"" = 'USA' LIMIT 1;");
+
+                var regionCalifornia = await connection.QueryFirstOrDefaultAsync<int>(
+                    @"SELECT ""Id"" FROM ""Region"" WHERE ""CountryId"" = @countryUSA AND ""Code"" = 'CA' LIMIT 1;",
+                    new { countryUSA });
+
+                await connection.ExecuteAsync(@"
+INSERT INTO ""Address""
+(
+      ""TenantId""
+    , ""EntityId""
+    , ""AddressType""
+    , ""StreetAddress1""
+    , ""City""
+    , ""RegionId""
+    , ""CountryId""
+    , ""PostalCode""
+    , ""CreatedById""
+)
+VALUES
+(
+     @_tenantId
+    ,@tenantEntityId
+    ,@addressType
+    ,'123 Some Street'
+    ,'Santa Ana'
+    ,@regionCalifornia
+    ,@countryUSA
+    ,'92701'
+    ,@userId
+);
+",
+                    new
+                    {
+                        addressType = (int)AddressType.Home,
+                        countryUSA,
+                        regionCalifornia,
+                        tenantEntityId,
+                        _tenantId,
+                        userId
+                    });
             }
         }
 
@@ -85,8 +142,16 @@ namespace DashAccountingSystemV2.Tests.Repositories
             {
                 var parameters = new { _tenantId };
 
-                await connection.ExecuteAsync(@"
-                    DELETE FROM ""Tenant"" WHERE ""Id"" = @_tenantId;",
+                await connection.ExecuteAsync(
+                    @"DELETE FROM ""Address"" WHERE ""TenantId"" = @_tenantId;",
+                    parameters);
+
+                await connection.ExecuteAsync(
+                    @"DELETE FROM ""Entity"" WHERE ""TenantId"" = @_tenantId;",
+                    parameters);
+
+                await connection.ExecuteAsync(
+                    @"DELETE FROM ""Tenant"" WHERE ""Id"" = @_tenantId;",
                     parameters);
             }
         }
