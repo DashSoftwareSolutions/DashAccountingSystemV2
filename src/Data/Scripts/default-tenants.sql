dcb057bb5bb143bb951aaf16bb7f92eb5f9be45c
Geoffrey Roberts <groberts314@yahoo.com>
2024-02-22 13:29:56 -0800

    fix: issue with the script (missing comma)
    DOH! =)

--- src/Data/Scripts/default-tenants.sql
+++ src/Data/Scripts/default-tenants.sql
@@ -14,7 +14,7 @@ BEGIN
         END IF;
 
         IF NOT EXISTS (SELECT 1 FROM "Tenant" WHERE LOWER("Name") = LOWER('Dash Software Solutions, Inc.') LIMIT 1) THEN
-            INSERT INTO "Tenant" ( "Name" "DefaultAssetTypeId" )
+            INSERT INTO "Tenant" ( "Name", "DefaultAssetTypeId" )
             VALUES ( 'Dash Software Solutions, Inc.', DEFAULT_ASSET_TYPE_USD );
         END IF;
     END IF;
