9b67636c0aaf92a1319868bc28e878de154bb481
Geoffrey Roberts <groberts314@yahoo.com>
2024-07-15 04:04:56 -0700

    feat: stub out invoice to prove invoice download works

--- src/BackEnd/Services/Export/DataExporters/InvoicePdfExporter.cs
+++ src/BackEnd/Services/Export/DataExporters/InvoicePdfExporter.cs
@@ -1,3 +1,5 @@
+using MigraDocCore.DocumentObjectModel;
+using MigraDocCore.Rendering;
 using DashAccountingSystemV2.BackEnd.Models;
 
 namespace DashAccountingSystemV2.BackEnd.Services.Export.DataExporters
@@ -8,9 +10,48 @@ namespace DashAccountingSystemV2.BackEnd.Services.Export.DataExporters
         {
             logger.LogInformation("Exporting Invoice {invoiceNumber} for Tenant {tenant}", data.InvoiceNumber, data.Tenant);
 
-            // TODO: Implement me!
+            var documentSubject = $"{data.Tenant.Name} - Invoice ${data.InvoiceNumber}";
 
-            return Task.FromResult<ExportedDataDto?>(null);
+            var document = new Document();
+            document.Info.Title = documentSubject;
+            document.Info.Author = "Dash Accounting System 2.1";
+            document.Info.Subject = documentSubject;
+
+            var section = document.AddSection();
+            var color = Colors.Black;
+
+            var paragraph = section.AddParagraph();
+            paragraph.Format.SpaceBefore = Unit.FromPoint(24);
+            paragraph.Format.Font.Color = color;
+            paragraph.Format.Font.Size = Unit.FromPoint(24);
+
+            paragraph.AddFormattedText(data.Tenant.Name, TextFormat.Bold);
+
+            paragraph = section.AddParagraph();
+            paragraph.Format.Font.Color = color;
+            paragraph.Format.Font.Size = Unit.FromPoint(24);
+            paragraph.AddFormattedText($"Fake Invoice #{data.InvoiceNumber}", TextFormat.Bold);
+
+            // TODO: build Real invoice
+
+            // Create a renderer for the MigraDocCore document.
+            var pdfRenderer = new PdfDocumentRenderer(unicode: true)
+            {
+                Document = document,
+            };
+
+            // Layout and render document to PDF
+            pdfRenderer.RenderDocument();
+
+            // Save the document and return the stream
+            var stream = new MemoryStream();
+            pdfRenderer.PdfDocument.Save(stream);
+
+            return Task.FromResult<ExportedDataDto?>(new ExportedDataDto()
+            {
+                Content = stream.ToArray(),
+                FileName = $"Invoice_{data.InvoiceNumber}",
+            });
         }
     }
 }
